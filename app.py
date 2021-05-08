c905eee25880b76d0fbd6cedd1dc8ad07d386b82
aaravdixite <83871387+aaravdixite@users.noreply.github.com>
2021-05-08 21:19:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import string
 from PIL import Image
 import os
